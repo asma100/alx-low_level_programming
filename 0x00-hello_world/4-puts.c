cf71e38273d0cb4558b199ad804f292df0e1ed0c
asma <asmaa.alhadi100@gmail.com>
2023-08-10 16:51:39 -0700

    1-6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void)
+{
+    char Taskc[] = "\"Programming is like building a multilingual puzzle";
+    
+    puts(Taskc); 
+    
+    return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+char a;
+int s;
+long int m;
+long long int e;
+float h;
+printf("Size of a char: %d byte(s)\n", sizeof(a));
+printf("Size of an int: %d byte(s)\n", sizeof(s));
+printf("Size of a long int: %d byte(s)\n", sizeof(m));
+printf("Size of a long long int: %d byte(s)\n", sizeof(a));
+printf("Size of a float: %d byte(s)\n", sizeof(a));
+return (0);
+}
