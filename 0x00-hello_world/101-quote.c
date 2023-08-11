f4562cf253227b0d3f145615670f816598148b9f
asma <asmaa.alhadi100@gmail.com>
2023-08-10 18:35:39 -0700

    101 task+

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,8 @@
  */
 int main(void)
 {
-char *task101 = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char *task101 = "and that piece of art is useful\" - Dora Korpar,
+2015-10-19\n";
 write(STDERR_FILENO, task101, 50);
-return 1;
+return (1);
 }
