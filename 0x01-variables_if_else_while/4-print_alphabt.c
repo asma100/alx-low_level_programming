dd6db10f9037a365cfa13cd3d174b07f19cb636d
asma <asmaa.alhadi100@gmail.com>
2023-08-11 15:10:59 -0700

    it is taskk no 4!

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char first = 'a';
+char end = 'z' ;
+char alpha = first ;
+ while(alpha <= end)
+  {
+if(alpha != 'e' && alpha != 'q')
+putchar(alpha);
+alpha++;
+  }
+ putchar('\n');
+return (0);
+}
