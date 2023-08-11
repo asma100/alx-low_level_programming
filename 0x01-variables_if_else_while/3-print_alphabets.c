4080d9edab8b46bcebcc3037d9d135c23aea6a14
asma <asmaa.alhadi100@gmail.com>
2023-08-11 14:55:27 -0700

    3!!

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet 
+ *
+ * Return:0
+ */
+int main(void)
+{
+char first = 'a';
+char end = 'z' ;
+char alpha = first ;
+char first2 = 'A';
+char end2 = 'Z' ;
+char alpha2 = first2 ;
+ while(alpha <= end)
+   {
+      
+     putchar(alpha);
+     alpha++;
+  }
+ while(alpha2 <= end2)
+  {
+     putchar(alpha2);
+     alpha2++;
+  }
+ putchar('\n');
+return (0);
+}
