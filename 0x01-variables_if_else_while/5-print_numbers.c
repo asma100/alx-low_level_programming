2ebfa16f5817b35fd26fe6e2b5ba7fd636bcce0b
asma <asmaa.alhadi100@gmail.com>
2023-08-11 17:27:52 -0700

    a

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,9 +8,9 @@
  */
 int main(void)
 {
-  int n;
-  for (n = 0; n < 10; n++)
-    printf("%d",n);
-  putchar('\n');
-  return (0);
+int n;
+for (n = 0; n < 10; n++)
+printf("%d",n);
+putchar('\n');
+return (0);
 }
