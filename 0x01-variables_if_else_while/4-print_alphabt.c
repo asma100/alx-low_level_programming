963b6d1e39502df94ce893d1bd977e9fd9f47c28
asma <asmaa.alhadi100@gmail.com>
2023-08-11 17:22:29 -0700

    a

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-  char first = 'a';
-  char end = 'z' ;
-  char alpha = first ;
-  while(alpha <= end)
-    {
-      if(alpha != 'e' && alpha != 'q')
-	putchar(alpha);
-      alpha++;
-    }
-  putchar('\n');
-  return (0);
+char first = 'a';
+char end = 'z';
+char alpha = first;
+while (alpha <= end)
+{
+if(alpha != 'e' && alpha != 'q')
+putchar(alpha);
+alpha++;
+}
+putchar('\n');
+return (0);
 }
