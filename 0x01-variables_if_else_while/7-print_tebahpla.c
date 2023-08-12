773ec7761847a504a66112ffe6d086a8e9b94a36
asma <asmaa.alhadi100@gmail.com>
2023-08-11 17:34:33 -0700

    a

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,10 +6,9 @@
  */
 int main(void)
 {
-  char alaph ;
-  
-  for (alaph= 'z'; alaph >= 'a'; alaph--)
-    putchar(alaph);	
-  putchar('\n');
-  return (0);
+char alaph ;
+for (alaph= 'z'; alaph >= 'a'; alaph--)
+putchar(alaph);	
+putchar('\n');
+return (0);
 }
