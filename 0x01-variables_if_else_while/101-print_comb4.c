cdcaf57effb93935e489b015cdea486f267706a6
asma <asmaa.alhadi100@gmail.com>
2023-08-14 10:20:27 -0700

    adv tasks

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,13 +5,13 @@
  */
 int main(void)
 {
-int i,j,k;
+int  i , j , k;
 
 for (i = 0; i < 9; i++)
 {
 for (j = i + 1; j <= 9; j++)
 {
-for (k = j+1; k <= 9; k++)
+for (k = j + 1; k <= 9; k++)
 {
 putchar(i + '0');
 putchar(j + '0');
