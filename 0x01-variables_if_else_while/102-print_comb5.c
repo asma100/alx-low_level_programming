cfa60d5f86848ffd3884fe7f6e9723562af20a41
asma <asmaa.alhadi100@gmail.com>
2023-08-14 16:20:21 -0700

    adv tasks

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,31 +7,24 @@
  */
 int main(void)
 {
-int i,j,k,m;
 
-for (i = 0; i <= 9; i++)
+int i, j;
+for (i = 0; i <= 98; i++)
 {
-for (j = 0 ; j <= i; j++)
+for (j = i + 1; j <= 99; j++)
 {
-for (k = j; k <= j; k++)
-{
-for (m = k+1 ; m <= k; m++)
-{
-putchar(i + '0');
-putchar(j + '0');
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
 putchar(' ');
-putchar(k + '0');
-putchar(m + '0');
-if (i != 9 || j != 9 || k != 9|| m != 9)
-{
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
+if (i == 98 && j == 99)
+continue;
 putchar(',');
 putchar(' ');
 }
 }
-}
-}
-}
 putchar('\n');
-
+putchar('\n');
 return (0);
 }
