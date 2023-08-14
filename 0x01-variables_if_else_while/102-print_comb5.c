6ecfb40c638cd5cd29313517166b5de190e60ffd
asma <asmaa.alhadi100@gmail.com>
2023-08-14 16:26:12 -0700

    adv tasks

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-
-/**                                                                                                                                                
+/**
  * main - Prints different combinations of two digits.
  *
  * Return:0.
