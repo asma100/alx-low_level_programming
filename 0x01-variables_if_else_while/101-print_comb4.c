1b6c55c36388b986ab01d200ac4822cf1a7694ad
asma <asmaa.alhadi100@gmail.com>
2023-08-14 10:15:28 -0700

    adv tasks

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
-
-/**                                                                                                                                                                    \
-                                                                                                                                                                        
- * main - Prints different combinations of two digits.                                                                                                                 \
-                                                                                                                                                                        
- *                                                                                                                                                                     \
-                                                                                                                                                                        
- * Return: Always 0.                                                                                                                                                   \
-                                                                                                                                                                        
+/**
+ *main - Prints different combinations of three digits.
+ *Return: Always 0
  */
 int main(void)
 {
@@ -17,7 +11,7 @@ for (i = 0; i < 9; i++)
 {
 for (j = i + 1; j <= 9; j++)
 {
-for (k = j; k < 9; k++)
+for (k = j+1; k < 9; k++)
 {
 putchar(i + '0');
 putchar(j + '0');
