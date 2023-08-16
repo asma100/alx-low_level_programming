7119325c53fe3f2da7f880a724fb05f549d4305b
asma <asmaa.alhadi100@gmail.com>
2023-08-15 19:22:31 -0700

    tasks

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,14 +4,14 @@
  * @c: is the char to be checked
  * Return: 1 if char is lowercase, otherwise 0.
  */
-int _islower(int c){
-    
-if(c >= 'a' && c <= 'z')
+int _islower(int c)
 {
-return 1;
+if (c >= 'a' && c <= 'z')
+{
+return (1);
 }
 else
 {
-return 0;
+return (0);
 }
 }
