9050b62ec4984c7b9f924f29831ecec57312ece8
asma <asmaa.alhadi100@gmail.com>
2023-08-15 18:58:12 -0700

    tasks

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,15 +4,16 @@
  *main-prints
  *Return:0
  */
-int main() {
-  _putchar('_');
-  _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
-  _putchar('\n');
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,9 +9,9 @@
 void print_alphabet(void)
 {
 char first = 'a';
-char end = 'z' ;
-char alpha = first ;
-while(alpha <= end)
+char end = 'z';
+char alpha = first;
+while ( alpha <= end )
 {
 _putchar(alpha);
 alpha++;
