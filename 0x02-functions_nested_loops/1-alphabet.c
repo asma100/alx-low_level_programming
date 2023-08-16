c71cf54a498e5cf51f0dacef9b468b24b804b461
asma <asmaa.alhadi100@gmail.com>
2023-08-15 18:49:38 -0700

    11

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
 /**
- * main - Prints the alphabet in lowercase.
- *
- * Return: Always 0.
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
+
 void print_alphabet(void)
 {
 char first = 'a';
 char end = 'z' ;
 char alpha = first ;
- while(alpha <= end)
-  {
-
-   _putchar(alpha);
-     alpha++;
-  }
-
+while(alpha <= end)
+{
+_putchar(alpha);
+alpha++;
+}
 _putchar('\n');
 }
-
