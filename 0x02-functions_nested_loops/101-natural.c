a69773a159191a89f122d71374fbc74b4046f1b4
asma <asmaa.alhadi100@gmail.com>
2023-08-16 08:31:42 -0700

    adv+2

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void  multiples_sum(void);
+
+
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    
+ multiples_sum();
+ 
+    return (0);
+}
+
+    
+void  multiples_sum(void)
+{
+   int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+}
