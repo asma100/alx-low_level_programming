8bf1cb7d5e48d747edebd8a3ad3d2f70626617f8
asma <asmaa.alhadi100@gmail.com>
2023-08-15 17:32:28 -0700

    8

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,6 +9,9 @@ int _isalpha(int c);
 int print_sign(int n);
 int print_last_digit(int);
 int _abs(int);
-
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
 #endif
 
