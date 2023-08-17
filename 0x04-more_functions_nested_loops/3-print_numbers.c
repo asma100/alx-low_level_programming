266c3fa7700ad84560eda42899417745f7727ec1
asma <asmaa.alhadi100@gmail.com>
2023-08-17 08:39:16 -0700

    1nest

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - Prints 0 from 9,
+ *
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+_putchar(n%10+'0');
+_putchar('\n');
+}
