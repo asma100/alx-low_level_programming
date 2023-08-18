627d0356398ec18e3a62a6b23fc91be48170399c
asma <asmaa.alhadi100@gmail.com>
2023-08-17 17:31:44 -0700

    nest

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,17 +5,19 @@
  */
 void print_diagonal(int n)
 {
-
 int i, j;
-for (i = 0; i <= n; i++)
+if (n <= 0)
 {
-_putchar('\\');
 _putchar('\n');
-for (j = n - i; j <= n; j++)
+return;
+}
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
 {
 _putchar(' ');
 }
-
+_putchar('\\');
+_putchar('\n');
 }
-
 }
