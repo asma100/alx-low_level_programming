00382dcfc84ae7415efe1bf108b0c6eabad82a01
asma <asmaa.alhadi100@gmail.com>
2023-08-17 18:19:12 -0700

    nest

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,25 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/**
- *main - positive or negative
- *
+#include "main.h"
+/** 
+ * positive_or_negative - prints positive or negative
+ *@i:number
  *Return:0
  */
 
-int main(void)
+void positive_or_negative(int i)
 {
-int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n < 0)
-printf("%d is negative\n", n);
+if (i > 0)
+printf("%d is positive\n", i);
+else if (i < 0)
+printf("%d is negative\n", i);
 else
-printf("%d is zero\n", n);
+printf("%d is zero\n", i);
 
-return (0);
+
 }
