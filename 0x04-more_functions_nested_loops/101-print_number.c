bac0cdd4159adc3c8b6e3cdf3438e2d8f25ba350
asma <asmaa.alhadi100@gmail.com>
2023-08-17 18:03:14 -0700

    nest

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,19 +1,28 @@
 #include "main.h"
 
+
 /**
  * print_number - prints an integer
  * @n: integer to be printed
  */
+
 void print_number(int n)
 {
-if (n < 0)
-{
-_putchar('-');
-n = -n;
-}
-if (n / 10)
-{
-print_number(n / 10);
-}
-_putchar(n % 10 + '0');
+	unsigned int m;
+
+	if (n < 0)
+	{
+		m = -n;
+		_putchar('-');
+	} else
+	{
+		m = n;
+	}
+
+	if (m / 10)
+	{
+		print_number(m / 10);
+	}
+
+	_putchar((m % 10) + '0');
 }
