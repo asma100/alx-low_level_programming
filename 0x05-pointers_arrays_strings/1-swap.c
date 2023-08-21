5010553db91b452e2ce6a7fccf7b4cfe3eb46875
asma <asmaa.alhadi100@gmail.com>
2023-08-21 14:39:13 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+ /**
+ * swap_int - swap number
+ * @a: The number one
+ * @b:The number two
+ * Return: On success 1.
+ *
+ */
+void swap_int(int *a, int *b){
+int m=*a;
+int n=*b;
+*a=n;
+*b=m;
+ }
+
+ 
