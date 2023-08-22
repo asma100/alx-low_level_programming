d1295c4e13ff7923ccb22ecb37061375cdb03ad1
asma <asmaa.alhadi100@gmail.com>
2023-08-21 17:47:48 -0700

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,11 @@
-#include "main.h" 
+#include "main.h"
  /**
  * reset_to_98 - reset n to 98
  * @n: The in to reset
  *
- * 
+ *
  */
  void reset_to_98(int *n){
-    
-*n= 98;
-    
+*n = 98;
 }
  
