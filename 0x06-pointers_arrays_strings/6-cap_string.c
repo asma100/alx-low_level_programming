808c2d0e33e93251e96663ec5af9524525078f64
asma <asmaa.alhadi100@gmail.com>
2023-08-25 13:54:54 -0700

    pointers, arrays and strings2tasls

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * string_toupper - change lowercase to uppercase if needed
+ * @u: upper output
+ *
+ * Return: u
+ */
+
+
+char *cap_string(char *u)
+{
+int i;
+i = 0;
+while (u[i] != '\0')
+{
+while (!(u[i] >= 'a' && u[i] <= 'z'))
+i++;
+if (u[i - 1] == ' ' || u[i - 1] == '\t' ||
+u[i - 1] == '\n' || u[i - 1] == ',' ||
+u[i - 1] == ';' || u[i - 1] == '.' ||
+u[i - 1] == '!' || u[i - 1] == '?' ||
+u[i - 1] == '"' || u[i - 1] == '(' ||
+u[i - 1] == ')' || u[i - 1] == '{' ||
+u[i - 1] == '}' || i == 0)
+u[i] = u[i] - 32;
+i++;
+}
+return (u);
+}
