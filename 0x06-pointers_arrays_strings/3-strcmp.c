1c4ecb10d68a152415ed90355ec986a1d0f72fdc
asma <asmaa.alhadi100@gmail.com>
2023-08-25 10:58:45 -0700

    pointers, arrays and strings2tasls

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
+ *
+ * Return: s1[i] - s2[i]
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int i;
+int j;
+i = 0;
+j = 0;
+while ((s1[i] && s2[j]) != '\0')
+{
+if (i == j)
+{
+return (0);
+}
+else
+{
+return (s1[i] - s2[j]);
+}
+
+i++;
+j++;
+}
+return (0);
+}
