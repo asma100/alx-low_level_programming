e44843e7e00ac7a23559d96c8f8df921dd797951
asma <asmaa.alhadi100@gmail.com>
2023-08-25 14:22:16 -0700

    pointers, arrays and strings2tasls

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * leet - change vowels to numbers.
+ * @u: analized string.
+ *
+ * Return: String with all vowels changed.
+ */
+char *leet(char *u)
+{
+	char x[] = "aeotlAEOTL";
+	char y[] = "4307143071";
+	int i = 0;
+	int j;
+
+	while (u[i] != '\0')
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (u[i] == x[j])
+			{
+				u[i] = y[j];
+			}
+		}
+		i++;
+	}
+	return (u);
+}
