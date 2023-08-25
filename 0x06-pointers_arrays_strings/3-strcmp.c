51418b009007121eb9ec8c6de8d6fc48ad4ee7ed
asma <asmaa.alhadi100@gmail.com>
2023-08-25 11:06:04 -0700

    pointers, arrays and strings2tasls

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,22 +9,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int i;
-int j;
-i = 0;
-j = 0;
-while ((s1[i] && s2[j]) != '\0')
-{
-if (i == j)
-{
-return (0);
-}
-else
-{
-return (s1[i] - s2[j]);
-}
+	int i;
 
-i++;
-j++;
-}
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
 }
