22601a88830d65394fb77a209fd539a6e880f9db
asma <asmaa.alhadi100@gmail.com>
2023-08-28 15:30:24 -0700

    mpointers, arrays and strings2tasls

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_memcpy -  copies memory area
+ *@dest:  stored here
+ *@src: copied here
+ *@n: number of bytes
+ *
+ *Return:dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int j = 0;
+while (j < n)
+{
+dest[j] = src[j];
+j++;
+}
+return (dest);
+}
