ad3ccefa9047cc39587f9a364ad2312b07b34c6a
asma <asmaa.alhadi100@gmail.com>
2023-08-28 12:51:41 -0700

    mpointers, arrays and strings2tasls

---
+++ 0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - fill memory with a specific value
+ * @dest: starting address of memory 
+ * @src: the desired value
+ * @n: number of bytes 
+ *
+ * Return: changed array with new value for n bytes
+ */
+char *_memset(char *s, char b, unsigned int n){
+int i = 0;
+int j = 0;
+while (i < 97 && j < n)
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+i++;
+}
