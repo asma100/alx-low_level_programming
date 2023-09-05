814a1ccdfee3fd92cfe6eef57ebd4713aa01bcc9
asma <asmaa.alhadi100@gmail.com>
2023-09-05 13:28:12 -0700

    Static malloctasks

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,59 +8,57 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-int i = 0;
-int j = 0;
-char *m;
-int l1, l2;
-if (s1 == NULL && s2 == NULL)
-return (NULL);
-if (s1 != NULL)
-l1 = _strlen(s1);
-if (s2 != NULL)
-l2 = _strlen(s2);
-m = malloc(sizeof(char) * (l1 + l2 + 1));
-if (m == NULL)
-return (NULL);
-if (s1 == NULL && s2 != NULL)
-while (s2[i])
-{
-m[i] = s2[i];
-i++;
+    int i = 0;
+    int j = 0;
+    char *m;
+    int l1 = 0;
+    int l2 = 0;
+
+    if (s1 != NULL)
+        l1 = _strlen(s1);
+    if (s2 != NULL)
+        l2 = _strlen(s2);
+
+    m = malloc(sizeof(char) * (l1 + l2 + 1));
+    if (m == NULL)
+        return (NULL);
+
+    if (s1 != NULL)
+    {
+        while (s1[i])
+        {
+            m[i] = s1[i];
+            i++;
+        }
+    }
+
+    if (s2 != NULL)
+    {
+        while (s2[j])
+        {
+            m[i] = s2[j];
+            i++;
+            j++;
+        }
+    }
+
+    m[i] = '\0';
+
+    return (m);
 }
-else if (s1 != NULL && s2 == NULL)
-{
-while (s1[j])
-{
-m[i] = s1[i];
-i++;
-}
-}
-else
-{
-while (s1[j])
-{
-m[i] = s1[j];
-i++;
-j++;
-}
-j = 0;
-while (s2[j])
-{
-m[i] = s2[j];
-i++;
-j++;
-}
-}
-m[i] = '\0';
-return (m);
-}
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
 int _strlen(char *s)
 {
-int cou = 0;
-while (*s != '\0')
-{
-s++;
-cou++;
-}
-return (cou);
+    int cou = 0;
+    while (*s != '\0')
+    {
+        s++;
+        cou++;
+    }
+    return (cou);
 }
