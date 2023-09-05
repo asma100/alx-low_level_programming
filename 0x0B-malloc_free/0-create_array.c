244d2348a35baa59eb760fab7d74323533734665
asma <asmaa.alhadi100@gmail.com>
2023-09-05 06:25:04 -0700

    Static malloctasks

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars,
+ * @size: size of array
+ * @c: char
+ * Return: pointer to array, NULL if fail
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+unsigned int i;
+char *m;
+m = malloc((size) * sizeof(char));
+if (m == NULL)
+{
+return (NULL);
+}
+i = 0;
+while (i < size)
+{
+m[i] = c;
+i++;
+}
+return (m);
+}
