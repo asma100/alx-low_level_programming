4208b3e0ea4856e41b33dbeb3523741220a2ea3a
asma <asmaa.alhadi100@gmail.com>
2023-09-06 10:28:41 -0700

    argc, argvtasks

---
+++ 0x0A-argc_argv/main.c
@@ -0,0 +1,27 @@
+int _atoi(char *s)
+{
+  int result = 0;
+  int sign = 1;
+
+  if (*s == '-')
+  {
+    sign = -1;
+    s++;
+  }
+
+  while (*s != '\0')
+  {
+    if (*s >= '0' && *s <= '9')
+    {
+      result = result * 10 + (*s - '0');
+    }
+    else
+    {
+      break;
+    }
+
+    s++;
+  }
+
+  return sign * result;
+}
