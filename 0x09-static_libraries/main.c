61f90b9a869c1206e6c0407707199caadfc04b6e
asma <asmaa.alhadi100@gmail.com>
2023-09-06 14:22:46 -0700

    Static librariestasks

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,12 @@
 #include "main.h"
-
+/**
+ * main - prints
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t")
+_puts("- Kevin Mitnick");
+return (0);
 }
