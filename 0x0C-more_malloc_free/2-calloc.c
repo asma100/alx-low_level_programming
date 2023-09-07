a56893dccee4c40213e77e0dc33ecdc47b794305
asma <asmaa.alhadi100@gmail.com>
2023-09-07 09:14:26 -0700

    2malloctasks

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,20 +11,21 @@ char *_memset(char *s, char b, unsigned int n);
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-unsigned int total_size = nmemb * size;
-char *ptr;
-if (nmemb == 0 || size == 0)
-{
-return (NULL);
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
 }
-ptr = malloc(total_size);
-if (ptr == NULL)
-{
-return (NULL);
-}
-_memset(ptr, 0, total_size);
-return (ptr);
-}
+
 /**
  * _memset - fill memory with a specific value
  * @s: starting address of memory
