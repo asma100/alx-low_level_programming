295883786da1c8494514f8886be0ae745d676bf4
asma <asmaa.alhadi100@gmail.com>
2023-09-07 09:18:48 -0700

    2malloctasks

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -35,13 +35,13 @@ return (ptr);
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i = 0;
-unsigned int j = 0;
-while (i < 97 && j < n)
-{
-s[i] = b;
-i++;
-j++;
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
 }
-return (s);
-}
+
