df90a7570f5b7edfcc051a5dc5a4770ae2ff6797
asma <asmaa.alhadi100@gmail.com>
2023-09-07 16:36:08 -0700

    2malloctasks

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,45 +11,40 @@ char *_memcpy(char *dest, char *src, unsigned int n);
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-void *new_ptr = malloc(new_size);
-if (ptr == NULL)
-{
-return (malloc(new_size));
-}
-if (new_size == 0)
-{
-free(ptr);
-return (NULL);
-}
-if (new_size == old_size)
-{
-return (ptr);
-}
+	char *ptr2;
+	char *ptr1;
+	unsigned int i;
 
-if (new_ptr == NULL)
-{
-return (NULL);
-}
-_memcpy(new_ptr, ptr, old_size < new_size ? old_size : new_size);
-free(ptr);
-return (new_ptr);
-}
-/**
- *_memcpy -  copies memory area
- *@dest:  stored here
- *@src: copied here
- *@n: number of bytes
- *
- *Return:dest
- */
+	if (new_size == old_size)
+		return (ptr);
 
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-unsigned int j = 0;
-while (j < n)
-{
-dest[j] = src[j];
-j++;
-}
-return (dest);
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (!ptr)
+		return (malloc(new_size));
+
+	ptr2 = malloc(new_size);
+	if (!ptr2)
+		return (NULL);
+
+	ptr1 = ptr;
+
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			ptr2[i] = ptr2[i];
+	}
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			ptr2[i] = ptr1[i];
+	}
+
+	free(ptr);
+	return (ptr2);
 }
