b0c93c31b0809811966b4f7345152da5d6544e5a
asma <asmaa.alhadi100@gmail.com>
2023-09-11 15:44:34 -0700

    structtasks

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees memory allocated for a dog struct
+ * @d: pointer to dog struct
+ */
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+return;
+free(d->name);
+free(d->owner);
+free(d);
+d = NULL;
+}
