ce3a3a45c3a1c94a0b3eb8377c471b89378bde3a
asma <asmaa.alhadi100@gmail.com>
2023-09-11 13:40:56 -0700

    structtasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,16 @@
+#ifndef Asma
+#define Asma
 /**
  * struct dog - dog withe the following elements:
- * @dog: First member
+ *
  * @name: Second member
  * @age: Third member
  * @owner: forth member
  */
-
-#ifndef Asma
-#define Asma
-struct dog {
-char * name;
-float age;
-char * owner;
+struct dog
+{
+char *name;
+float  age;
+char *owner;
 };
 #endif
