9d4bbc976a24006028973a0e43c28a799098bd26
asma <asmaa.alhadi100@gmail.com>
2023-09-21 15:57:03 -0700

    first tasks in linked lists edit 1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,24 +1,23 @@
 #include "lists.h"
 /**
  * print_list - print list nodes
- *@list_t:the list
+ *
  * @h:pointer to list
  * Return: size_t
  */
 
 
-size_t print_list(const list_t *h){
+size_t print_list(const list_t *h)
+{
 int count = 0;
-
-const list_t *p = h;
-while (p != NULL)
+while (h != NULL)
 {
 count++;
-if (p->str == NULL)
+if (h->str == NULL)
 printf("[0] (nil)\n");
 else
-printf("[%d]%s\n", p->len, p->str);
-p = p->next;
+printf("[%d]%s\n", h->len, h->str);
+h = h->next;
 }
-return count;
+return (count);
 }
