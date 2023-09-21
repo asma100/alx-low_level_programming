78d38d24b49e45b0aa6233d9a4cadb915143ee9e
asma <asmaa.alhadi100@gmail.com>
2023-09-21 16:05:30 -0700

    first tasks in linked lists edit 4 count loc

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,12 +12,12 @@ size_t print_list(const list_t *h)
 size_t count = 0;
 while (h != NULL)
 {
-count++;
 if (h->str == NULL)
 printf("[0] (nil)\n");
 else
 printf("[%d]%s\n", h->len, h->str);
 h = h->next;
+count ++;
 }
 return (count);
 }
