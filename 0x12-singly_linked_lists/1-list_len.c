01d648c70084d5617292560bd8065e2f8867e6e4
asma <asmaa.alhadi100@gmail.com>
2023-09-21 18:08:54 -0700

    task 1e in linked lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,14 +10,12 @@
 
 size_t list_len(const list_t *h)
 {
-    size_t count = 0;
-    const list_t *current = h;
-
-    while (current != NULL)
-    {
-        count++;
-        current = current->next;
-    }
-
-    return count;
+size_t count = 0;
+const list_t *current = h;
+while (current != NULL)
+{
+count++;
+current = current->next;
+}
+return (count);
 }
