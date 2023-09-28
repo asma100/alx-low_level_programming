e0ba64bc94fba4d105db93ab183bb736a5f21fa7
asma <asmaa.alhadi100@gmail.com>
2023-09-28 16:59:02 -0700

    task 3

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,7 +7,12 @@
  */
 int get_bit(unsigned long int x, unsigned int index)
 {
-unsigned long int mask = 1;
+unsigned long int mask;
+if (index >= sizeof(unsigned long int) * 8)
+{
+return (-1);
+}
+mask = 1;
 mask <<= index;
 return ((x & mask) ? 1 : 0);
 }
