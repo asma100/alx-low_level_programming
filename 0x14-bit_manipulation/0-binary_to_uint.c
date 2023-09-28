df015e284ea3fc2071909c587d1a4524ebb5d56a
asma <asmaa.alhadi100@gmail.com>
2023-09-28 15:03:45 -0700

    first editng betty + b being NULL

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
  *
@@ -16,7 +17,7 @@ if (b[i] == '1')
 {
 n += 1 << (len - i - 1);
 }
-else if (b[i] != '0')
+else if (b[i] != '0' || b == NULL)
 {
 return (0);
 }
@@ -29,7 +30,7 @@ return (n);
  *
  * Return: the length of the string
  */
-int _strlen ( const  char *s)
+int _strlen(const  char *s)
 {
 int cou = 0;
 while (*s != '\0')
