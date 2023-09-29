a86984554e5a02ca05682afb8afb6241b2a515d9
asma <asmaa.alhadi100@gmail.com>
2023-09-28 17:50:41 -0700

    task4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * clear_bit - clear the value of a bit to 0 at a given index.
+ * @index:the position
+ * @n:unsigned long int
+ * Return: int
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int mask;
+if (index >= sizeof(unsigned long int) * 8)
+{
+return (-1);
+}
+mask = ~(1 << index);
+*n &= mask;
+return (1);
+}
