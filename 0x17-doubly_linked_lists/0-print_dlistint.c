a8210832cb8a7a231911f4b38717eaac8bb86d69
asma <asmaa.alhadi100@gmail.com>
2023-11-09 10:22:48 -0800

    cdlist

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,14 +1,27 @@
 #include "lists.h"
+/**
+ * print_dlistint - prints list
+ * dlistint_t list
+ *
+ * @h: pointer
+ * Return: number of nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-const dlistint_t *p = h;
-size_t count = 0;
+int  count = 0;
+const dlistint_t *p;
 if (h == NULL)
 return (count);
+
+while (h->prev != NULL)
+		h = h->prev;
+p = h;
 while (p != NULL)
 {
 if (h != NULL)
 printf("%d \n", p->n);
+else
+printf("[0] (nil)\n");
 count += 1;
 p = p->next;
 }
