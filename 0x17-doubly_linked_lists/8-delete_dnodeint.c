0db3c49e5a48a640d188f5a5b29211a08c7c0e43
asma <asmaa.alhadi100@gmail.com>
2023-11-09 15:58:50 -0800

    cdlist

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - get dnodeint at index
+ *
+ * @index: int
+ *
+ * Return: value
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+dlistint_t *current = *head;
+unsigned int i;
+if (*head == NULL)
+return (-1);
+while (current != NULL && current->prev != NULL)
+current = current->prev;
+i = 0;
+while (current != NULL)
+{
+if (i == index)
+{
+if (i == 0)
+{
+*head = current->next;
+if (current->next != NULL)
+current->next->prev = NULL;
+}
+else
+{
+current->prev->next = current->next;
+if (current->next != NULL)
+current->next->prev = current->prev;
+}
+free(current);
+return (1);
+}
+current = current->next;
+i++;
+}
+return (-1);
+}
