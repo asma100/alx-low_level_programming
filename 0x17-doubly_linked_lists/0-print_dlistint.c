d4d277044af95ee9090d074592cad0fddca30ff9
asma <asmaa.alhadi100@gmail.com>
2023-11-09 10:16:24 -0800

    cdlist

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,13 +3,12 @@ size_t print_dlistint(const dlistint_t *h)
 {
 const dlistint_t *p = h;
 size_t count = 0;
-
+if (h == NULL)
+return (count);
 while (p != NULL)
 {
-if (p->n)
+if (h != NULL)
 printf("%d \n", p->n);
-else
-printf("[0] (nil)\n");
 count += 1;
 p = p->next;
 }
