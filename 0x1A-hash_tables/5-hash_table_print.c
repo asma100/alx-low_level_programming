2de3dcb8d3fe383a3c3cced8a59d6bb9d88681aa
asma <asmaa.alhadi100@gmail.com>
2023-12-03 15:33:28 -0800

    py

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,21 +11,25 @@
  **/
 void hash_table_print(const hash_table_t *ht)
 {
-unsigned long int i;
-hash_node_t *node = NULL;
-if (!ht || ht->size == 0)
-{
-return;
-}
-printf("{");
-for (i = 0; i < ht->size; i++)
-{
-node = ht->array[i];
-while (node)
-{
-printf("'%s': '%s', ", node->key, node->value);
-node = node->next;
-}
-}
-printf("}\n");
+ unsigned long int i;
+ hash_node_t *node;
+ int first = 1;
+
+ if (ht == NULL)
+  return;
+
+ printf("{");
+ for (i = 0; i < ht->size; i++)
+ {
+  node = ht->array[i];
+  while (node != NULL)
+  {
+   if (!first)
+    printf(", ");
+   printf("'%s': '%s'", node->key, node->value);
+   first = 0;
+   node = node->next;
+  }
+ }
+ printf("}\n");
 }
