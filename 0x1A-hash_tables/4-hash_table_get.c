b58bd92aa79c663a1572498293ee56894cd064bf
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-02-04 15:40:53 -0800

    py

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -2,12 +2,12 @@
 #include <stddef.h>
 #include <string.h>
 #include "hash_tables.h"
-/**                                                                                                                                                                     
- * hash_table_get - retrieves a value associated with a key                                                                                                             
- * @ht: hash table to look into                                                                                                                                         
- * @key: key to look for                                                                                                                                                
- *                                                                                                                                                                      
- * Return: value associated with the element, or NULL if key couldn't be found                                                                                          
+/**
+ * hash_table_get - retrieves a value
+ * @ht: hash table to look into
+ * @key: key
+ *
+ * Return: value
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
@@ -17,7 +17,7 @@ int index;
 unsigned long int hash_value;
 if (ht == NULL || key == NULL || strlen(key) == 0)
 {
-return NULL;
+return (NULL);
 }
 
 hash_value = hash_djb2((const unsigned char *)key);
@@ -27,9 +27,9 @@ while (current != NULL)
 {
 if (strcmp(current->key, key) == 0)
 {
-return current->value;
+return (current->value);
 }
 current = current->next;
 }
-return NULL;
+return (NULL);
 }
