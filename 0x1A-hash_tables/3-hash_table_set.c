1ae79bd58c177698f4ef5bbd2c2c12cf57e2f5a0
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-02-04 10:15:13 -0800

    py

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,25 +12,27 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-unsigned long int hash_value = hash_djb2((const unsigned char *)key);
-int index = hash_value % ht->size;
-hash_node_t *new_node = malloc(sizeof(hash_node_t));
-new_node->key = strdup(key);
-new_node->value = strdup(value);
-new_node->next = NULL;
 
-if (ht->array[index] == NULL)
-{
-ht->array[index] = new_node;
-}
-else
-{
-hash_node_t *current = ht->array[index];
-while (current->next != NULL) {
-current = current->next;
-}
-current->next = new_node;
-}
+    unsigned long int hash_value;
+    hash_node_t *new_node;
+    int index;
+    if (key == NULL || strlen(key) == 0)
+    {
+        return 0;
+    }
 
-return 1;
+    hash_value = hash_djb2((const unsigned  char *)key);
+    index = hash_value % ht->size;
+
+    new_node = malloc(sizeof(hash_node_t));
+    if (new_node == NULL)
+    {
+        return 0;
+    }
+    new_node->key = strdup(key);
+    new_node->value = strdup(value);
+    new_node->next = ht->array[index];
+    ht->array[index] = new_node;
+
+    return 1;
 }
