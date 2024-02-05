38a974e0f3b674ab53cda0e2cb4b3053c4a2ae18
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-02-04 16:22:40 -0800

    py

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,28 +1,28 @@
 #include <stdlib.h>
 #include "hash_tables.h"
+/**
+ * hash_table_create - hash_table_create
+ * @tableSize: unsigned long int
+ * Return: hashTablePointer
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  unsigned long int i;
-  hash_table_t *ht = malloc(sizeof(hash_table_t));
-  if (ht == NULL)
-    {
-      return NULL;
-    }
-
-  ht->size = size;
-  ht->array = malloc(sizeof(hash_node_t *) * size);
-
-  if (ht->array == NULL)
-    {
-      free(ht);
-      return NULL;
-    }
-
-
-  for (i = 0; i < size; i++)
-    {
-      ht->array[i] = NULL;
-    }
-
-  return ht;
+unsigned long int i;
+hash_table_t *ht = malloc(sizeof(hash_table_t));
+if (ht == NULL)
+{
+return (NULL);
+}
+ht->size = size;
+ht->array = malloc(sizeof(hash_node_t *) * size);
+if (ht->array == NULL)
+{
+free(ht);
+return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+ht->array[i] = NULL;
+}
+return (ht);
 }
